fc93f79f076532770bbbf7f6012cacac75751561
CosmeNicolas <nicomas7590@gmail.com>
2024-01-09 12:01:59 -0300

    inicialice el proyecto turnos veterinaria

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Container} from 'react-bootstrap'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Container className="main">
+       <h1>Administracion Veterinaria 🐕</h1>
+      </Container>
+    </>
+  )
+}
+
+export default App
