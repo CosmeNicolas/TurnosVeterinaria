3f0f8b038f9db0c474b50e826aaa33ce5dc3f97f
CosmeNicolas <nicomas7590@gmail.com>
2024-01-09 19:17:51 -0300

    agregue el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+
 import {Container} from 'react-bootstrap'
 import Footer from './components/Footer'
+import Formulario from './components/Formulario'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+
 
 function App() {
  
+  
+    
 
   return (
     <>
       <Container  className="main text-center">
         <h1>Administracion Veterinaria Peluditos🐕</h1>
+        <Formulario/>
       </Container>
       <Footer/>
     </>
