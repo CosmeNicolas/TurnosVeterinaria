8097d7a2ac6dc664a3502156f8c0a3dcc012133f
CosmeNicolas <nicomas7590@gmail.com>
2024-01-10 01:28:41 -0300

    actualice la rama dev

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,85 +1,111 @@
- import { useState } from 'react' 
-import { Form, Button} from "react-bootstrap";
-import TurnosVista from './TurnosVista';
+import { useState, useEffect } from "react";
+import { Form, Button } from "react-bootstrap";
+import TurnosVista from './TurnosVista'
 
 
 const Formulario = () => {
-  const [nombreMascota, setNombreMascota] = useState('')
-  const [nombreDueño, setNombreDueno] = useState('')
-  const [fecha, setFecha] = useState('')
-  const [hora, setHora] = useState('')
+  const [nombreMascota, setNombreMascota] = useState('');
+  const [nombreDueno, setNombreDueno] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [hora, setHora] = useState('');
+  const [turno, setTurno] = useState('');
+  const turnosGuardados = JSON.parse(localStorage.getItem('guardarTurno')) || [];
+  const [turnos, setTurnos] = useState(turnosGuardados);
+
+  console.log(turnos)
+
+  useEffect(() => {
+    localStorage.setItem('guardarTurno', JSON.stringify(turnos))
+  }, [turnos])
 
 
-  const handleSubmit = (e)=> {
+  const borrarTurno = (nombreTurno) => {
+    const tomarTurno = turnos.filter((turno) => turno !== nombreTurno);
+    setTurnos(tomarTurno)
+  }
+
+  const handleSubmit = (e) => {
     e.preventDefault();
 
+    const nuevoTurno = {
+      nombreMascota,
+      nombreDueno,
+      fecha,
+      hora
+    }
+
+    setTurnos([...turnos, nuevoTurno])
+    setTurno(turno);
+
+    setNombreMascota('');
+    setNombreDueno('');
+    setFecha('');
+    setHora('');
   }
 
 
-  return(
+  return (
     <>
-    
-    <div className="container container-form">
+      <div className="container container-form">
 
-      <Form>
-        {/* nombre mascota */}
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Nombre Mascota</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Ingrese el nombre de su mascota"
-            name='nombreMascota'
-            minLength={2}
-            maxLength={50}
-            value={nombreMascota}
-            onChange={(e)=>setNombreMascota(e.target.value)}
-            required />
-        </Form.Group>
-        {/* nombre dueño */}
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Nombre Dueño</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Ingrese el nombre de su mascota"
-            name='nombre'
-            minLength={2}
-            maxLength={50}
-            value={nombreDueño}
-            onChange={(e)=>setNombreDueno(e.target.value)}
-            required />
-        </Form.Group>
-        {/* fecha */}
-        <Form.Group className="mb-3" controlId="formFecha">
-          <Form.Label>Fecha</Form.Label>
-          <Form.Control
-            type="date"
-            name='fecha'
-            value={fecha}
-            onChange={(e)=>setFecha(e.target.value)}
-            required
-          />
-        </Form.Group>
-        {/* hora */}
-       
-        <Form.Group className="mb-3" controlId="formHora">
-          <Form.Label>Hora</Form.Label>
-          <Form.Control
-            type="time"
-            name='hora'
-            value={hora}
-            onChange={(e)=>setHora(e.target.value)}
-            required
-          />
-        </Form.Group>
+        <Form onSubmit={handleSubmit}>
+          {/* nombre mascota */}
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Nombre Mascota</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese el nombre de su mascota"
+              name='nombreMascota'
+              minLength={2}
+              maxLength={50}
+              value={nombreMascota}
+              onChange={(e) => setNombreMascota(e.target.value)}
+              required />
+          </Form.Group>
+          {/* nombre dueño */}
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Nombre Dueño</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese el nombre de su mascota"
+              name='nombre'
+              minLength={2}
+              maxLength={50}
+              value={nombreDueno}
+              onChange={(e) => setNombreDueno(e.target.value)}
+              required />
+          </Form.Group>
+          {/* fecha */}
+          <Form.Group className="mb-3" controlId="formFecha">
+            <Form.Label>Fecha</Form.Label>
+            <Form.Control
+              type="date"
+              name='fecha'
+              value={fecha}
+              onChange={(e) => setFecha(e.target.value)}
+              required
+            />
+          </Form.Group>
+          {/* hora */}
 
-        <Button variant="dark" type="submit" className='btn-form py-2 my-2'>
-          Crear Turno
-        </Button>
-      </Form>
-    </div>
-    <TurnosVista/>
-    
-        </>
+          <Form.Group className="mb-3" controlId="formHora">
+            <Form.Label>Hora</Form.Label>
+            <Form.Control
+              type="time"
+              name='hora'
+              value={hora}
+              onChange={(e) => setHora(e.target.value)}
+              required
+            />
+          </Form.Group>
+
+          <Button variant="dark" type="submit" className='btn-form py-2 my-2'>
+            Crear Turno
+          </Button>
+        </Form>
+      </div>
+      <TurnosVista turnos={turnos} borrarTurno={borrarTurno} />
+    </>
   )
 }
 
--- src/components/Turno.jsx
+++ src/components/Turno.jsx
@@ -1,21 +1,30 @@
-import { Button, ListGroup, Card } from 'react-bootstrap';
+import { Container, Button, ListGroup, Card } from 'react-bootstrap';
 
-const Turno = () => {
+const Turno = ({ turnos, borrarTurno }) => {
   return (
     <>
-      <Card.Body>
-        <Card.Title>Turno</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Mascota</Card.Subtitle>
-      <ListGroup variant="flush" className='text-start my-2'>
-        <ListGroup.Item>Nombre Mascota</ListGroup.Item>
-        <ListGroup.Item>Nombre Dueño</ListGroup.Item>
-        <ListGroup.Item>Fecha</ListGroup.Item>
-        <ListGroup.Item>Hora</ListGroup.Item>
-      </ListGroup>
-      <Button variant="dark" className='btn-borrar py-2'>
-        Borrar Turno
-      </Button>
-      </Card.Body>
+      <Container>
+        {turnos.map((itemTurno, posicionItemTturno) => (
+          <Card.Body key={posicionItemTturno}>
+            <Card.Title>Turno</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Mascota</Card.Subtitle>
+            <ListGroup variant="flush" className='text-start'>
+              <ListGroup.Item>Nombre mascota: {itemTurno.nombreMascota}</ListGroup.Item>
+              <ListGroup.Item>Nombre Dueño: {itemTurno.nombreDueno}</ListGroup.Item>
+              <ListGroup.Item>Fecha: {itemTurno.fecha}</ListGroup.Item>
+              <ListGroup.Item>Hora: {itemTurno.hora}</ListGroup.Item>
+            </ListGroup>
+            <Button onClick={() => borrarTurno(itemTurno)} variant="dark" className='btn-borrar py-2'>
+              Borrar Turno
+            </Button>
+          </Card.Body>
+        ))}
+        {turnos.length === 0 && (
+          <Card.Body>
+            <Card.Title>Ningún turno cargado</Card.Title>
+          </Card.Body>
+        )}
+      </Container>
     </>
   );
 };
