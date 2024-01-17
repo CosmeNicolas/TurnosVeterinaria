f36561bd39e1f0f29e5df6713d61e97ce09c691a
CosmeNicolas <nicomas7590@gmail.com>
2024-01-17 13:56:02 -0300

    le di formato al componente de turnos

--- src/components/Turno.jsx
+++ src/components/Turno.jsx
@@ -1,5 +1,5 @@
 import Swal from 'sweetalert2';
-import {Container, Button, ListGroup, Card } from 'react-bootstrap';
+import { Container, Button, ListGroup, Card } from 'react-bootstrap';
 
 const Turno = ({ turnos, borrarTurno }) => {
   console.log(turnos)
@@ -39,33 +39,33 @@ const Turno = ({ turnos, borrarTurno }) => {
 
   return (
     <>
-        <Container>
-      {
-        turnos.map((itemTurno ,posicionItemTturno)=>(
-          <Card key={posicionItemTturno} className='my-3'>
-          <Card.Body >
-              <Card.Title>Turno</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Mascota</Card.Subtitle>
-              <ListGroup variant="flush" className='text-start'>
-              <ListGroup.Item>Nombre mascota: {itemTurno.nombreMascota}</ListGroup.Item>
-              <ListGroup.Item>Nombre Dueño: {itemTurno.nombreDueno}</ListGroup.Item>
-              <ListGroup.Item>Fecha: {itemTurno.fecha}</ListGroup.Item>
-              <ListGroup.Item>Hora: {itemTurno.hora}</ListGroup.Item>
-                <ListGroup.Item>Sintomas: {itemTurno.sintoma}</ListGroup.Item>
-              </ListGroup>
-              <Button onClick={() => confirmarBorrarTurno(itemTurno)} variant="dark" className='btn-borrar py-2'>
-                Borrar Turno
-              </Button>
-          </Card.Body>
-          </Card>
-        ))}
-     
-          {turnos.length === 0 && (
+      <Container>
+        {
+          turnos.map((itemTurno, posicionItemTturno) => (
+            <Card key={posicionItemTturno} className='my-3'>
+              <Card.Body >
+                <Card.Title>Turno</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Mascota</Card.Subtitle>
+                <ListGroup variant="flush" className='text-start'>
+                  <ListGroup.Item>Nombre mascota: {itemTurno.nombreMascota}</ListGroup.Item>
+                  <ListGroup.Item>Nombre Dueño: {itemTurno.nombreDueno}</ListGroup.Item>
+                  <ListGroup.Item>Fecha: {itemTurno.fecha}</ListGroup.Item>
+                  <ListGroup.Item>Hora: {itemTurno.hora}</ListGroup.Item>
+                  <ListGroup.Item>Sintomas: {itemTurno.sintoma}</ListGroup.Item>
+                </ListGroup>
+                <Button onClick={() => confirmarBorrarTurno(itemTurno)} variant="dark" className='btn-borrar py-2'>
+                  Borrar Turno
+                </Button>
+              </Card.Body>
+            </Card>
+          ))}
+
+        {turnos.length === 0 && (
           <Card.Body>
             <Card.Title className='py-3 my-3 bg bg-body-tertiary border-title'>No hay turnos cargados</Card.Title>
           </Card.Body>
-          )}
-    </Container>
+        )}
+      </Container>
     </>
   );
 };
